aa5ed235d607d5c1d684c8442e9565719b7328ec
Gustavo Caris <gustavocaris14052002@gmail.com>
2025-10-31 17:52:21 -0300

    Nomeando as variavbeis e comecnado o projeto Screen sound

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+//screen sound
+string boaVindas = "Seja bem vindo a aula de C#!";
+void ExibirMensagem(string mensagem)
+{
+    Console.WriteLine(mensagem);
+}
+
+
+Console.WriteLine(boaVindas);
+
+Console.WriteLine(boaVindas);
+Console.WriteLine(boaVindas);
